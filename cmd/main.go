9843e897f00612354fd7a2e4f8bc8d76fba12be6
cx-monicac <monica.casanova@checkmarx.com>
2023-03-23 05:02:58 +0000

    feat: rules filter cli argument (#15)

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"github.com/checkmarx/2ms/plugins"
 	"github.com/checkmarx/2ms/reporting"
-	"github.com/checkmarx/2ms/wrapper"
+	"github.com/checkmarx/2ms/secrets"
 	"strings"
 
 	"github.com/rs/zerolog"
@@ -48,7 +48,7 @@ func initLog() {
 func Execute() {
 	cobra.OnInitialize(initLog)
 	rootCmd.Flags().BoolP("all", "", true, "scan all plugins")
-	rootCmd.Flags().BoolP("all-rules", "", true, "all rules")
+	rootCmd.Flags().StringSlice("tags", []string{"all"}, "select rules to be applied")
 
 	for _, plugin := range allPlugins {
 		err := plugin.DefineCommandLineArgs(rootCmd)
@@ -64,11 +64,27 @@ func Execute() {
 	}
 }
 
+func validateTags(tags []string) {
+	for _, tag := range tags {
+		if !(strings.EqualFold(tag, "all") || strings.EqualFold(tag, secrets.TagApiKey) || strings.EqualFold(tag, secrets.TagClientId) ||
+			strings.EqualFold(tag, secrets.TagClientSecret) || strings.EqualFold(tag, secrets.TagSecretKey) || strings.EqualFold(tag, secrets.TagAccessKey) ||
+			strings.EqualFold(tag, secrets.TagAccessId) || strings.EqualFold(tag, secrets.TagApiToken) || strings.EqualFold(tag, secrets.TagAccessToken) ||
+			strings.EqualFold(tag, secrets.TagRefreshToken) || strings.EqualFold(tag, secrets.TagPrivateKey) || strings.EqualFold(tag, secrets.TagPublicKey) ||
+			strings.EqualFold(tag, secrets.TagEncryptionKey) || strings.EqualFold(tag, secrets.TagTriggerToken) || strings.EqualFold(tag, secrets.TagRegistrationToken) ||
+			strings.EqualFold(tag, secrets.TagPassword) || strings.EqualFold(tag, secrets.TagUploadToken) || strings.EqualFold(tag, secrets.TagPublicSecret) ||
+			strings.EqualFold(tag, secrets.TagSensitiveUrl) || strings.EqualFold(tag, secrets.TagWebhook)) {
+			log.Fatal().Msgf(`invalid filter: %s`, tag)
+		}
+	}
+}
+
 func execute(cmd *cobra.Command, args []string) {
-	allRules, err := cmd.Flags().GetBool("all-rules")
+	tags, err := cmd.Flags().GetStringSlice("tags")
 	if err != nil {
 		log.Fatal().Msg(err.Error())
 	}
+
+	validateTags(tags)
 
 	// -------------------------------------
 	// Get content from plugins
@@ -99,15 +115,13 @@ func execute(cmd *cobra.Command, args []string) {
 	// -------------------------------------
 	// Detect Secrets
 
-	if allRules {
-		wrap := wrapper.NewWrapper()
+	secrets := secrets.Init(tags)
 
-		for _, item := range items {
-			secrets := wrap.Detect(item.Content)
-			report.Results[item.ID] = append(report.Results[item.ID], secrets...)
-		}
-		report.TotalItemsScanned = len(items)
+	for _, item := range items {
+		secrets := secrets.Detect(item.Content)
+		report.Results[item.ID] = append(report.Results[item.ID], secrets...)
 	}
+	report.TotalItemsScanned = len(items)
 
 	// -------------------------------------
 	// Show Report
