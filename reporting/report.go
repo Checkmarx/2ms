43caab38ac6e14c451634d59ba760b2c5950744b
cx-monicac <monica.casanova@checkmarx.com>
2023-05-05 14:32:25 +0100

    feat: adds repository plugin (#62)
    Co-authored-by: Baruch Odem <baruch.odem@checkmarx.com>

--- reporting/report.go
+++ reporting/report.go
@@ -2,6 +2,7 @@ package reporting
 
 import (
 	"fmt"
+	"path/filepath"
 	"strings"
 )
 
@@ -41,9 +42,9 @@ func (r *Report) ShowReport() {
 
 func (r *Report) generateResultsReport() {
 	for source, secrets := range r.Results {
-		itemLink := getItemId(source)
-		fmt.Printf("- Item ID: %s\n", itemLink)
-		fmt.Printf(" - Item Link: %s\n", source)
+		itemId := getItemId(source)
+		fmt.Printf("- Item ID: %s\n", itemId)
+		fmt.Printf(" - Item Full Path: %s\n", source)
 		fmt.Println("  - Secrets:")
 		for _, secret := range secrets {
 			fmt.Printf("   - Type: %s\n", secret.Description)
@@ -53,7 +54,13 @@ func (r *Report) generateResultsReport() {
 }
 
 func getItemId(fullPath string) string {
-	itemLinkStrings := strings.Split(fullPath, "/")
-	itemLink := itemLinkStrings[len(itemLinkStrings)-1]
-	return itemLink
+	var itemId string
+	if strings.Contains(fullPath, "/") {
+		itemLinkStrings := strings.Split(fullPath, "/")
+		itemId = itemLinkStrings[len(itemLinkStrings)-1]
+	}
+	if strings.Contains(fullPath, "\\") {
+		itemId = filepath.Base(fullPath)
+	}
+	return itemId
 }
