d65be5b81e6a90fd28cce667b1b9a667a52944f1
Baruch Odem <baruch.odem@checkmarx.com>
2023-05-29 12:47:23 +0300

    read root folders

---
+++ lib/paligo-api.go
@@ -0,0 +1,95 @@
+package lib
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Item struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	UUID string `json:"uuid"`
+	Type string `json:"type"`
+}
+
+type Folder struct {
+	Item
+	Children []Item `json:"children"`
+}
+
+type EmptyFolder struct {
+	Item
+	Children string `json:"children"`
+}
+
+type Component struct {
+	Item
+	Subtype          string        `json:"subtype"`
+	Creator          int           `json:"creator"`
+	Owner            int           `json:"owner"`
+	Author           int           `json:"author"`
+	CreatedAt        int           `json:"created_at"`
+	ModifiedAt       int           `json:"modified_at"`
+	Checkout         bool          `json:"checkout"`
+	CheckoutUser     string        `json:"checkout_user"`
+	ParentResource   int           `json:"parent_resource"`
+	Taxonomies       []interface{} `json:"taxonomies"`
+	ReleaseStatus    string        `json:"release_status"`
+	Content          string        `json:"content"`
+	Languages        []string      `json:"languages"`
+	External         []interface{} `json:"external"`
+	CustomAttributes []interface{} `json:"custom_attributes"`
+}
+
+type ListFoldersResponse struct {
+	Page       int           `json:"page"`
+	NextPage   string        `json:"next_page"`
+	TotalPages int           `json:"total_pages"`
+	Folders    []EmptyFolder `json:"folders"`
+}
+
+type Paligo struct {
+	Instance string
+	Username string
+	Token    string
+}
+
+func (p *Paligo) GetCredentials() (string, string) {
+	return p.Username, p.Token
+}
+
+func (p *Paligo) ListFolders() (*[]EmptyFolder, error) {
+	url := fmt.Sprintf("https://%s.paligoapp.com/api/v2/folders", p.Instance)
+
+	req, err := HttpRequest("GET", url, p)
+	if err != nil {
+		return nil, err
+	}
+
+	var folders *ListFoldersResponse
+	err = json.Unmarshal(req, &folders)
+
+	return &folders.Folders, err
+}
+
+func (p *Paligo) ShowFolder(folderId string) (*Folder, error) {
+	url := fmt.Sprintf("https://%s.paligoapp.com/api/v2/folders/%s", p.Instance, folderId)
+
+	req, err := HttpRequest("GET", url, p)
+	if err != nil {
+		return nil, err
+	}
+
+	var folder *Folder
+	err = json.Unmarshal(req, folder)
+
+	return folder, err
+}
+
+func NewPaligoApi(instance string, username string, token string) *Paligo {
+	return &Paligo{
+		Instance: instance,
+		Username: username,
+		Token:    token,
+	}
+}
---
+++ plugins/paligo.go
@@ -0,0 +1,116 @@
+package plugins
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/checkmarx/2ms/lib"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+const (
+	paligoInstanceFlag = "instance"
+	paligoUsernameFlag = "username"
+	paligoTokenFlag    = "token"
+	paligoFolderFlag   = "folder"
+)
+
+var (
+	paligoInstanceArg string
+	paligoFolderArg   string
+
+	foldersChan   = make(chan lib.Item)
+	componentChan = make(chan lib.Component)
+)
+
+type PaligoPlugin struct {
+	Plugin
+	Channels
+	username string
+	token    string
+}
+
+func (p *PaligoPlugin) GetCredentials() (string, string) {
+	return p.username, p.token
+}
+
+func (p *PaligoPlugin) GetName() string {
+	return "paligo"
+}
+
+func (p *PaligoPlugin) DefineCommand(channels Channels) (*cobra.Command, error) {
+	p.Channels = channels
+
+	command := &cobra.Command{
+		Use: fmt.Sprintf("%s --%s %s --%s %s --%s %s",
+			p.GetName(),
+			paligoInstanceFlag, strings.ToUpper(paligoInstanceFlag),
+			paligoUsernameFlag, strings.ToUpper(paligoUsernameFlag),
+			paligoTokenFlag, strings.ToUpper(paligoTokenFlag)),
+		Short: "Scan Paligo instance",
+		Long:  "Scan Paligo instance for sensitive information.",
+		Run: func(cmd *cobra.Command, args []string) {
+			log.Info().Msg("Paligo plugin started")
+			p.getItems()
+		},
+	}
+
+	command.Flags().StringVar(&paligoInstanceArg, paligoInstanceFlag, "", "Paligo instance name [required]")
+	err := command.MarkFlagRequired(paligoInstanceFlag)
+	if err != nil {
+		return nil, fmt.Errorf("error while marking flag %s as required: %w", paligoInstanceFlag, err)
+	}
+	command.Flags().StringVar(&p.username, paligoUsernameFlag, "", "Paligo username [required]")
+	err = command.MarkFlagRequired(paligoUsernameFlag)
+	if err != nil {
+		return nil, fmt.Errorf("error while marking flag %s as required: %w", paligoUsernameFlag, err)
+	}
+	command.Flags().StringVar(&p.token, paligoTokenFlag, "", "Paligo token [required]")
+	err = command.MarkFlagRequired(paligoTokenFlag)
+	if err != nil {
+		return nil, fmt.Errorf("error while marking flag %s as required: %w", paligoTokenFlag, err)
+	}
+	command.Flags().StringVar(&paligoFolderArg, paligoFolderFlag, "", "Paligo folder ID")
+
+	return command, nil
+}
+
+func (p *PaligoPlugin) getItems() {
+	paligoApi := lib.NewPaligoApi(paligoInstanceArg, p.username, p.token)
+
+	p.Channels.WaitGroup.Add(1)
+	go p.handleFolders()
+
+	if paligoFolderArg != "" {
+		folder, err := paligoApi.ShowFolder(paligoFolderArg)
+		if err != nil {
+			log.Error().Err(err).Msg("error while getting folder")
+			p.Channels.Errors <- err
+		}
+		for _, folder := range folder.Children {
+			if folder.Type == "component" {
+				log.Info().Msgf("Found %s %s", folder.Type, folder.Name)
+			} else {
+				foldersChan <- folder
+			}
+		}
+	} else {
+		folders, err := paligoApi.ListFolders()
+		if err != nil {
+			log.Error().Err(err).Msg("error while getting folders")
+			p.Channels.Errors <- err
+		}
+		for _, folder := range *folders {
+			foldersChan <- folder.Item
+		}
+	}
+}
+
+func (p *PaligoPlugin) handleFolders() {
+	defer p.Channels.WaitGroup.Done()
+
+	for folder := range foldersChan {
+		log.Info().Msgf("Found folder %s", folder.Name)
+	}
+}
