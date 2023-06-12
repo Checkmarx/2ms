e5daef2f2eb1af37deab857038dda3786d79d365
Baruch Odem (Rothkoff) <baruch.odem@checkmarx.com>
2023-06-12 11:48:14 +0300

    feat: support scanning historical git commits (#88)
    - refactor: repository plugin initialization
    - change repository to filesystem
    - repository plugin - support scanning historical git commits
      Fixes #66

---
+++ plugins/filesystem.go
@@ -0,0 +1,102 @@
+package plugins
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+const flagFolder = "path"
+
+var ignoredFolders = []string{".git"}
+
+type FileSystemPlugin struct {
+	Plugin
+	Path string
+}
+
+func (p *FileSystemPlugin) GetName() string {
+	return "filesystem"
+}
+
+func (p *FileSystemPlugin) DefineCommand(channels Channels) (*cobra.Command, error) {
+	var cmd = &cobra.Command{
+		Use:   fmt.Sprintf("%s --%s PATH", p.GetName(), flagFolder),
+		Short: "Scan local folder",
+		Long:  "Scan local folder for sensitive information",
+		Run: func(cmd *cobra.Command, args []string) {
+			log.Info().Msg("Folder plugin started")
+			p.getFiles(channels.Items, channels.Errors, channels.WaitGroup)
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVar(&p.Path, flagFolder, "", "Local folder path [required]")
+	if err := cmd.MarkFlagDirname(flagFolder); err != nil {
+		return nil, fmt.Errorf("error while marking '%s' flag as directory: %w", flagFolder, err)
+	}
+	if err := cmd.MarkFlagRequired(flagFolder); err != nil {
+		return nil, fmt.Errorf("error while marking '%s' flag as required: %w", flagFolder, err)
+	}
+
+	return cmd, nil
+}
+
+func (p *FileSystemPlugin) getFiles(items chan Item, errs chan error, wg *sync.WaitGroup) {
+	fileList := make([]string, 0)
+	err := filepath.Walk(p.Path, func(path string, fInfo os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatal().Err(err).Msg("error while walking through the directory")
+		}
+		for _, ignoredFolder := range ignoredFolders {
+			if fInfo.Name() == ignoredFolder && fInfo.IsDir() {
+				return filepath.SkipDir
+			}
+		}
+		if fInfo.Size() == 0 {
+			return nil
+		}
+		if !fInfo.IsDir() {
+			fileList = append(fileList, path)
+		}
+		return err
+	})
+
+	if err != nil {
+		log.Fatal().Err(err).Msg("error while walking through the directory")
+	}
+
+	p.getItems(items, errs, wg, fileList)
+}
+
+func (p *FileSystemPlugin) getItems(items chan Item, errs chan error, wg *sync.WaitGroup, fileList []string) {
+	for _, filePath := range fileList {
+		wg.Add(1)
+		go func(filePath string) {
+			actualFile, err := p.getItem(wg, filePath)
+			if err != nil {
+				errs <- err
+				return
+			}
+			items <- *actualFile
+		}(filePath)
+	}
+}
+
+func (p *FileSystemPlugin) getItem(wg *sync.WaitGroup, filePath string) (*Item, error) {
+	defer wg.Done()
+	b, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	content := &Item{
+		Content: string(b),
+		ID:      filePath,
+	}
+	return content, nil
+}
---
+++ plugins/git.go
@@ -0,0 +1,85 @@
+package plugins
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gitleaks/go-gitdiff/gitdiff"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+	"github.com/zricethezav/gitleaks/v8/detect/git"
+)
+
+type GitPlugin struct {
+	Plugin
+	Channels
+}
+
+func (p *GitPlugin) GetName() string {
+	return "git"
+}
+
+func (p *GitPlugin) DefineCommand(channels Channels) (*cobra.Command, error) {
+	p.Channels = channels
+
+	command := &cobra.Command{
+		Use:   fmt.Sprintf("%s <PATH>", p.GetName()),
+		Short: "Scan Git repository",
+		Long:  "Scan Git repository for sensitive information.",
+		Args:  cobra.MatchAll(cobra.ExactArgs(1), validGitRepoArgs),
+		Run: func(cmd *cobra.Command, args []string) {
+			log.Info().Msg("Git plugin started")
+			scanGit(args[0], channels.Items, channels.Errors)
+		},
+	}
+
+	return command, nil
+}
+
+func scanGit(path string, itemsChan chan Item, errChan chan error) {
+	fileChan, err := git.GitLog(path, "")
+	if err != nil {
+		errChan <- fmt.Errorf("error while scanning git repository: %w", err)
+	}
+	log.Debug().Msgf("scanned git repository: %s", path)
+
+	for file := range fileChan {
+		log.Debug().Msgf("file: %s; Commit: %s", file.NewName, file.PatchHeader.Title)
+		if file.IsBinary || file.IsDelete {
+			continue
+		}
+
+		fileChanges := ""
+		for _, textFragment := range file.TextFragments {
+			if textFragment != nil {
+				raw := textFragment.Raw(gitdiff.OpAdd)
+				fileChanges += raw
+			}
+		}
+		if fileChanges != "" {
+			itemsChan <- Item{
+				Content: fileChanges,
+				ID:      fmt.Sprintf("git show %s:%s", file.PatchHeader.SHA, file.NewName),
+			}
+		}
+	}
+}
+
+func validGitRepoArgs(cmd *cobra.Command, args []string) error {
+	stat, err := os.Stat(args[0])
+	if err != nil {
+		return err
+	}
+	if !stat.IsDir() {
+		return fmt.Errorf("%s is not a directory", args[0])
+	}
+	gitFolder := fmt.Sprintf("%s/.git", args[0])
+	stat, err = os.Stat(gitFolder)
+	if err != nil {
+		return err
+	}
+	if !stat.IsDir() {
+		return fmt.Errorf("%s is not a git repository", args[0])
+	}
+	return nil
+}
