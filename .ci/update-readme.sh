043d533fbeaadd0493e7c66aef4773608e858eac
Baruch Odem <baruch.odem@checkmarx.com>
2023-06-29 15:33:39 +0300

    validate readme as dedicated workflow

---
+++ .ci/update-readme.sh
@@ -0,0 +1,11 @@
+help_message=$(go run .)
+echo '```' >output.txt
+echo "$help_message" >>output.txt
+echo '```' >>output.txt
+sed -i '/<!-- command-line:start -->/,/<!-- command-line:end -->/{
+            /<!-- command-line:start -->/{
+              p
+              r output.txt
+            }
+            /<!-- command-line:end -->/!d
+          }' README.md
