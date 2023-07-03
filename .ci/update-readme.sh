8f77eac6b9b07b1781b74dcf0e44d0df3dc3ee60
Baruch Odem (Rothkoff) <baruch.odem@checkmarx.com>
2023-07-03 10:28:09 +0300

    ci: update readme with help message (#123)
    If the `help` message in the README file is outdated, you will get an
    error and you will have to update the README.
    
    
    ![image](https://github.com/Checkmarx/2ms/assets/17686879/a083ff80-8c7e-42c5-b1e2-a40d54eda0b0)

---
+++ .ci/update-readme.sh
@@ -0,0 +1,17 @@
+help_message=$(go run .)
+
+echo '```' >output.txt
+echo "$help_message" >>output.txt
+echo '```' >>output.txt
+
+sed -i '/<!-- command-line:start -->/,/<!-- command-line:end -->/{
+            /<!-- command-line:start -->/{
+              p
+              r output.txt
+            }
+            /<!-- command-line:end -->/!d
+          }' README.md
+
+rm output.txt
+
+git --no-pager diff README.md
