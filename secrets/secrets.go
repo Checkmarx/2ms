1e8d7500e1fc3414296588ab19e3e7efb1892176
Baruch Odem (Rothkoff) <baruch.odem@checkmarx.com>
2023-09-10 16:40:36 +0300

    feat: change the include and exclude to select and ignore (#177)
    BREAKING CHANGE:
    `--include-rule` renamed to `--rule`
    `--exclude-rule` renamed to `--ignore-rule`
    Now you can use them together
    
    - redundant error return
    
    Part of #174

--- secrets/secrets.go
+++ secrets/secrets.go
@@ -52,26 +52,27 @@ const TagWebhook = "webhook"
 
 const customRegexRuleIdFormat = "custom-regex-%d"
 
-func Init(includeList, excludeList []string) (*Secrets, error) {
-	if len(includeList) > 0 && len(excludeList) > 0 {
-		return nil, fmt.Errorf("cannot use both include and exclude flags")
+func Init(selectedList, ignoreList []string) (*Secrets, error) {
+	if len(selectedList) > 0 && len(ignoreList) > 0 {
+		log.Warn().Msgf("Both 'rule' and 'ignoreRule' flags were provided.")
 	}
 
-	allRules, _ := loadAllRules()
-	rulesToBeApplied := make(map[string]config.Rule)
-	if len(includeList) > 0 {
-		rulesToBeApplied = selectRules(allRules, includeList)
-	} else if len(excludeList) > 0 {
-		rulesToBeApplied = excludeRules(allRules, excludeList)
-	} else {
-		for _, rule := range allRules {
-			// required to be empty when not running via cli. otherwise rule will be ignored
-			rule.Rule.Keywords = []string{}
-			rulesToBeApplied[rule.Rule.RuleID] = rule.Rule
-		}
+	selectedRules := loadAllRules()
+	if len(selectedList) > 0 {
+		selectedRules = selectRules(selectedRules, selectedList)
 	}
-	if len(rulesToBeApplied) == 0 {
+	if len(ignoreList) > 0 {
+		selectedRules = ignoreRules(selectedRules, ignoreList)
+	}
+	if len(selectedRules) == 0 {
 		return nil, fmt.Errorf("no rules were selected")
+	}
+
+	rulesToBeApplied := make(map[string]config.Rule)
+	for _, rule := range selectedRules {
+		// required to be empty when not running via cli. otherwise rule will be ignored
+		rule.Rule.Keywords = []string{}
+		rulesToBeApplied[rule.Rule.RuleID] = rule.Rule
 	}
 
 	config := config.Config{
@@ -144,30 +145,26 @@ func isSecretIgnored(secret *reporting.Secret, ignoredIds *[]string) bool {
 	return false
 }
 
-func selectRules(allRules []Rule, tags []string) map[string]config.Rule {
-	rulesToBeApplied := make(map[string]config.Rule)
+func selectRules(allRules []Rule, tags []string) []Rule {
+	selectedRules := []Rule{}
 
 	for _, rule := range allRules {
 		if isRuleMatch(rule, tags) {
-			// required to be empty when not running via cli. otherwise rule will be ignored
-			rule.Rule.Keywords = []string{}
-			rulesToBeApplied[rule.Rule.RuleID] = rule.Rule
+			selectedRules = append(selectedRules, rule)
 		}
 	}
-	return rulesToBeApplied
+	return selectedRules
 }
 
-func excludeRules(allRules []Rule, tags []string) map[string]config.Rule {
-	rulesToBeApplied := make(map[string]config.Rule)
+func ignoreRules(allRules []Rule, tags []string) []Rule {
+	selectedRules := []Rule{}
 
 	for _, rule := range allRules {
 		if !isRuleMatch(rule, tags) {
-			// required to be empty when not running via cli. otherwise rule will be ignored
-			rule.Rule.Keywords = []string{}
-			rulesToBeApplied[rule.Rule.RuleID] = rule.Rule
+			selectedRules = append(selectedRules, rule)
 		}
 	}
-	return rulesToBeApplied
+	return selectedRules
 }
 
 func isRuleMatch(rule Rule, tags []string) bool {
@@ -184,7 +181,7 @@ func isRuleMatch(rule Rule, tags []string) bool {
 	return false
 }
 
-func loadAllRules() ([]Rule, error) {
+func loadAllRules() []Rule {
 	var allRules []Rule
 	allRules = make([]Rule, 0)
 
@@ -346,7 +343,7 @@ func loadAllRules() ([]Rule, error) {
 	allRules = append(allRules, Rule{Rule: *rules.ZendeskSecretKey(), Tags: []string{TagSecretKey}})
 	allRules = append(allRules, Rule{Rule: *internalRules.AuthenticatedURL(), Tags: []string{TagSensitiveUrl}})
 
-	return allRules, nil
+	return allRules
 }
 
 var RulesCommand = &cobra.Command{
@@ -355,10 +352,7 @@ var RulesCommand = &cobra.Command{
 	Long:  `List all rules`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		rules, err := loadAllRules()
-		if err != nil {
-			return err
-		}
+		rules := loadAllRules()
 
 		tab := tabwriter.NewWriter(os.Stdout, 1, 2, 2, ' ', 0)
 
@@ -367,7 +361,7 @@ var RulesCommand = &cobra.Command{
 		for _, rule := range rules {
 			fmt.Fprintf(tab, "%s\t%s\t%s\n", rule.Rule.RuleID, rule.Rule.Description, strings.Join(rule.Tags, ","))
 		}
-		if err = tab.Flush(); err != nil {
+		if err := tab.Flush(); err != nil {
 			return err
 		}
 
