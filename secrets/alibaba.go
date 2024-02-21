a58ee0c6e997be1309a9458054127e791e040844
Baruch Odem (Rothkoff) <baruch.odem@checkmarx.com>
2024-02-21 12:48:52 +0200

    feat: validate pairs of secrets (#210)
    After adding the simple validation process on #206, I'm now adding a
    validation process for cases where both _access key_ and _secret key_
    are needed together.
    
    For these cases, the engine will collect those secrets and after the
    scan is finished, it will validate all the pairs.

---
+++ secrets/alibaba.go
@@ -0,0 +1,89 @@
+package secrets
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+// https://www.alibabacloud.com/help/en/sdk/alibaba-cloud-api-overview
+// https://www.alibabacloud.com/help/en/sdk/product-overview/rpc-mechanism#sectiondiv-y9b-x9s-wvp
+
+func validateAlibaba(secrets pairsByRuleId) {
+
+	accessKeys := secrets["alibaba-access-key-id"]
+	secretKeys := secrets["alibaba-secret-key"]
+
+	for _, accessKey := range accessKeys {
+		accessKey.ValidationStatus = Unknown
+
+		for _, secretKey := range secretKeys {
+			status, err := alibabaRequest(accessKey.Value, secretKey.Value)
+			if err != nil {
+				log.Warn().Err(err).Str("service", "alibaba").Msg("Failed to validate secret")
+			}
+
+			secretKey.ValidationStatus = status
+			if accessKey.ValidationStatus.CompareTo(status) == second {
+				accessKey.ValidationStatus = status
+			}
+		}
+	}
+}
+
+func alibabaRequest(accessKey, secretKey string) (validationResult, error) {
+	req, err := http.NewRequest("GET", "https://ecs.aliyuncs.com/", nil)
+	if err != nil {
+		return Unknown, err
+	}
+
+	// Workaround for gitleaks returns the key ends with "
+	// https://github.com/gitleaks/gitleaks/pull/1350
+	accessKey = strings.TrimSuffix(accessKey, "\"")
+	secretKey = strings.TrimSuffix(secretKey, "\"")
+
+	params := req.URL.Query()
+	params.Add("AccessKeyId", accessKey)
+	params.Add("Action", "DescribeRegions")
+	params.Add("SignatureMethod", "HMAC-SHA1")
+	params.Add("SignatureNonce", strconv.FormatInt(time.Now().UnixNano(), 10))
+	params.Add("SignatureVersion", "1.0")
+	params.Add("Timestamp", time.Now().UTC().Format(time.RFC3339))
+	params.Add("Version", "2014-05-26")
+
+	stringToSign := "GET&%2F&" + url.QueryEscape(params.Encode())
+	hmac := hmac.New(sha1.New, []byte(secretKey+"&"))
+	hmac.Write([]byte(stringToSign))
+	signature := base64.StdEncoding.EncodeToString(hmac.Sum(nil))
+
+	params.Add("Signature", signature)
+	req.URL.RawQuery = params.Encode()
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return Unknown, err
+	}
+	log.Debug().Str("service", "alibaba").Int("status_code", resp.StatusCode)
+
+	// If the access key is invalid, the response will be 404
+	// If the secret key is invalid, the response will be 400 along with other signautre Errors
+	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusBadRequest {
+		return Revoked, nil
+	}
+
+	if resp.StatusCode == http.StatusOK {
+		return Valid, nil
+	}
+
+	err = fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	return Unknown, err
+}
--- secrets/secret.go
+++ secrets/secret.go
@@ -1,20 +1,7 @@
 package secrets
 
-import (
-	"fmt"
-	"net/http"
-	"sync"
-
-	"github.com/rs/zerolog/log"
-)
-
-type ValidationResult string
-
-const (
-	Valid   ValidationResult = "Valid"
-	Revoked ValidationResult = "Revoked"
-	Unknown ValidationResult = "Unknown"
-)
+// TODO: rename package to engine and move secrets into subpackage
+// Then move the validators into a subpackage too
 
 type Secret struct {
 	ID               string           `json:"id"`
@@ -25,49 +12,16 @@ type Secret struct {
 	StartColumn      int              `json:"startColumn"`
 	EndColumn        int              `json:"endColumn"`
 	Value            string           `json:"value"`
-	ValidationStatus ValidationResult `json:"validationStatus,omitempty"`
-}
-
-type validationFunc = func(*Secret) ValidationResult
-
-var ruleIDToFunction = map[string]validationFunc{
-	"github-fine-grained-pat": validateGithub,
-	"github-pat":              validateGithub,
+	ValidationStatus validationResult `json:"validationStatus,omitempty"`
 }
 
 func isCanValidateRule(ruleID string) bool {
-	_, ok := ruleIDToFunction[ruleID]
-	return ok
-}
-
-func (s *Secret) Validate(wg *sync.WaitGroup) {
-	defer wg.Done()
-	if f, ok := ruleIDToFunction[s.RuleID]; ok {
-		s.ValidationStatus = f(s)
-	} else {
-		s.ValidationStatus = Unknown
+	if _, ok := ruleIDToFunction[ruleID]; ok {
+		return true
 	}
-}
-
-func validateGithub(s *Secret) ValidationResult {
-	const githubURL = "https://api.github.com/"
-
-	req, err := http.NewRequest("GET", githubURL, nil)
-	if err != nil {
-		log.Warn().Err(err).Msg("Failed to validate secret")
-		return Unknown
-	}
-	req.Header.Set("Authorization", fmt.Sprintf("token %s", s.Value))
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Warn().Err(err).Msg("Failed to validate secret")
-		return Unknown
+	if _, ok := ruleToGeneralKey[ruleID]; ok {
+		return true
 	}
 
-	if resp.StatusCode == http.StatusOK {
-		return Valid
-	}
-	return Revoked
+	return false
 }
