2d08f80968e918382e52bd7a76b79a8fdc7785a8
LeonardoLordelloFontes <leonardo.fontes@checkmarx.com>
2025-02-10 14:31:10 +0000

    Added test for preRun

---
+++ cmd/main_test.go
@@ -0,0 +1,87 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/checkmarx/2ms/engine"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPreRun(t *testing.T) {
+	tests := []struct {
+		name               string
+		stdoutFormatVar    string
+		reportPath         []string
+		engineConfigVar    engine.EngineConfig
+		customRegexRuleVar []string
+		validateVar        bool
+		expectedErr        error
+	}{
+		{
+			name:               "error in validateFormat",
+			stdoutFormatVar:    "invalid",
+			reportPath:         []string{"report.json"},
+			engineConfigVar:    engine.EngineConfig{},
+			customRegexRuleVar: []string{},
+			validateVar:        false,
+			expectedErr:        fmt.Errorf("invalid output format: invalid, available formats are: json, yaml and sarif"),
+		},
+		{
+			name:            "error in engine.Init",
+			stdoutFormatVar: "json",
+			reportPath:      []string{"mock.json"},
+			engineConfigVar: engine.EngineConfig{
+				SelectedList: []string{"mockInvalid"},
+			},
+			customRegexRuleVar: []string{},
+			validateVar:        false,
+			expectedErr:        fmt.Errorf("no rules were selected"),
+		},
+		{
+			name:               "error in engine.AddRegexRules",
+			stdoutFormatVar:    "json",
+			reportPath:         []string{"mock.json"},
+			engineConfigVar:    engine.EngineConfig{},
+			customRegexRuleVar: []string{"[a-z"},
+			validateVar:        false,
+			expectedErr:        fmt.Errorf("failed to compile regex rule [a-z: error parsing regexp: missing closing ]: `[a-z`"),
+		},
+		{
+			name:               "successfully started go routines with validateVar enabled",
+			stdoutFormatVar:    "json",
+			reportPath:         []string{"mock.json"},
+			engineConfigVar:    engine.EngineConfig{},
+			customRegexRuleVar: []string{},
+			validateVar:        false,
+			expectedErr:        nil,
+		},
+		{
+			name:               "successfully started go routines with validateVar disabled",
+			stdoutFormatVar:    "json",
+			reportPath:         []string{"mock.json"},
+			engineConfigVar:    engine.EngineConfig{},
+			customRegexRuleVar: []string{},
+			validateVar:        false,
+			expectedErr:        nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			stdoutFormatVar = tt.stdoutFormatVar
+			reportPathVar = tt.reportPath
+			engineConfigVar = tt.engineConfigVar
+			customRegexRuleVar = tt.customRegexRuleVar
+			validateVar = tt.validateVar
+			channels.Errors = make(chan error)
+			err := preRun("mock", nil, nil)
+			if tt.expectedErr != nil {
+				assert.Error(t, err)
+				assert.EqualError(t, err, tt.expectedErr.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.Empty(t, channels.Errors)
+			}
+		})
+	}
+}
