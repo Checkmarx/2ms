6ace3824b387f9f7ed1a816eda55bf16d74f0be2
Miguel Neiva <Miguel.Neiva@checkmarx.com>
2025-04-11 10:25:47 +0000

    feat: improve generic api key (#280)
    Closes #
    
    **Proposed Changes**
    
    
    https://checkmarx.atlassian.net/browse/AST-91330
    
    Ticket above close, improved generic-api-key to support xml format
    
    
    **Checklist**
    
    - [✔️ ] I covered my changes with tests.
    - [✔️ ] I Updated the documentation that is affected by my changes:
      - [ ✔️] Change in the CLI arguments
      - [ ✔️] Change in the configuration file
    
    I submit this contribution under the Apache-2.0 license.
    
    ---------
    
    Co-authored-by: Diogo-fj-rocha <diogo.fj.rocha@gmail.com>

--- engine/rules/utils.go
+++ engine/rules/utils.go
@@ -15,15 +15,17 @@ const (
 	identifierCaseInsensitiveSuffix = `)`
 	identifierPrefix                = `(?:`
 	identifierSuffix                = `)(?:[0-9a-z\-_\t .]{0,20})(?:[\s|']|[\s|"]){0,3}`
+	identifierSuffixIncludingXml    = `)(?:[0-9a-z\-_\t .]{0,20})(?:<\/key>\s{0,10}<string)?(?:[\s|']|[\s|"]){0,3}`
 
 	// commonly used assignment operators or function call
 	operator = `(?:=|>|:{1,3}=|\|\|:|<=|=>|:|\?=)`
 
 	// boundaries for the secret
 	// \x60 = `
-	secretPrefixUnique = `\b(`
-	secretPrefix       = `(?:'|\"|\s|=|\x60){0,5}(`
-	secretSuffix       = `)(?:['|\"|\n|\r|\s|\x60|;]|$)`
+	secretPrefixUnique       = `\b(`
+	secretPrefix             = `(?:'|\"|\s|=|\x60){0,5}(`
+	secretSuffix             = `)(?:['|\"|\n|\r|\s|\x60|;]|$)`
+	secretSuffixIncludingXml = `)(?:['|\"|\n|\r|\s|\x60|;]|$|\s{0,10}<\/string>)`
 )
 
 func generateSemiGenericRegex(identifiers []string, secretRegex string, isCaseInsensitive bool) *regexp.Regexp {
@@ -43,6 +45,31 @@ func generateSemiGenericRegex(identifiers []string, secretRegex string, isCaseIn
 	sb.WriteString(secretRegex)
 	sb.WriteString(secretSuffix)
 	return regexp.MustCompile(sb.String())
+}
+func generateSemiGenericRegexIncludingXml(identifiers []string, secretRegex string, isCaseInsensitive bool) *regexp.Regexp {
+	var sb strings.Builder
+	// The identifiers should always be case-insensitive.
+	// This is inelegant but prevents an extraneous `(?i:)` from being added to the pattern; it could be removed.
+	if isCaseInsensitive {
+		sb.WriteString(caseInsensitive)
+		writeIdentifiersIncludinXml(&sb, identifiers)
+	} else {
+		sb.WriteString(identifierCaseInsensitivePrefix)
+		writeIdentifiers(&sb, identifiers)
+		sb.WriteString(identifierCaseInsensitiveSuffix)
+	}
+	sb.WriteString(operator)
+	sb.WriteString(secretPrefix)
+	sb.WriteString(secretRegex)
+	sb.WriteString(secretSuffixIncludingXml)
+
+	return regexp.MustCompile(sb.String())
+}
+
+func writeIdentifiersIncludinXml(sb *strings.Builder, identifiers []string) {
+	sb.WriteString(identifierPrefix)
+	sb.WriteString(strings.Join(identifiers, "|"))
+	sb.WriteString(identifierSuffixIncludingXml)
 }
 
 func writeIdentifiers(sb *strings.Builder, identifiers []string) {
