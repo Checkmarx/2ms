30ef187166b85e74c6994f6985ed3f4beef74e52
Júlio Silva <158049359+cx-julio-silva@users.noreply.github.com>
2025-05-09 16:04:38 +0100

    Improved test structure

--- lib/reporting/sarif_test.go
+++ lib/reporting/sarif_test.go
@@ -8,6 +8,8 @@ import (
 )
 
 func TestCreateMessageText(t *testing.T) {
+	messagePrefix := "Test Rule has detected secret for file %s."
+	
 	tests := []struct {
 		Name            string
 		RuleName        string
@@ -16,33 +18,33 @@ func TestCreateMessageText(t *testing.T) {
 	}{
 		{
 			Name:            "Filesystem file name",
-			RuleName:        "Test Rule",
+			RuleName:        ruleName,
 			FilePath:        "folder/filename.txt",
-			ExpectedMessage: "Test Rule has detected secret for file folder/filename.txt.",
+			ExpectedMessage: sprintf(messagePrefix, "folder/filename.txt"),
 		},
 		{
 			Name:            "Simple git filename",
-			RuleName:        "Test Rule 2",
+			RuleName:        ruleName,
 			FilePath:        "git show 1a9f3c87b4d029f54e8c72d8b11a78f6a3c29d2e:folder/filename.txt",
-			ExpectedMessage: "Test Rule 2 has detected secret for file folder/filename.txt.",
+			ExpectedMessage: sprintf(messagePrefix, "folder/filename.txt"),
 		},
 		{
 			Name:            "Broken git file name with no commit hash",
-			RuleName:        "Test Rule 3",
+			RuleName:        ruleName,
 			FilePath:        "git show folder/filename.txt",
-			ExpectedMessage: "Test Rule 3 has detected secret for file git show folder/filename.txt.",
+			ExpectedMessage: sprintf(messagePrefix, "folder/filename.txt"),
 		},
 		{
 			Name:            "Git file name with one colon character",
-			RuleName:        "Test Rule 4",
+			RuleName:        ruleName,
 			FilePath:        "git show d8e914f06d8d4494bd4f9ab2a2c9c88f78ef25ad:folder/filename:secondpart.txt",
-			ExpectedMessage: "Test Rule 4 has detected secret for file folder/filename:secondpart.txt.",
+			ExpectedMessage: sprintf(messagePrefix, "folder/filename:secondpart.txt"),
 		},
 		{
 			Name:            "Git file name with multiple colon character",
-			RuleName:        "Test Rule 5",
+			RuleName:        ruleName,
 			FilePath:        "git show a73b5cf94f0b29e1cc6e71a092f6b8ebc1d0e002:folder:secondfolderpart/filename:secondpart.txt",
-			ExpectedMessage: "Test Rule 5 has detected secret for file folder:secondfolderpart/filename:secondpart.txt.",
+			ExpectedMessage: sprintf(messagePrefix, "folder:secondfolderpart/filename:secondpart.txt"),
 		},
 	}
 
