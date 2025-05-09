a7ee11eec4b841b176879ad17a9c8fd7c82ff8fc
Júlio Silva <158049359+cx-julio-silva@users.noreply.github.com>
2025-05-09 16:14:36 +0100

    removed unnecessary test field

--- lib/reporting/sarif_test.go
+++ lib/reporting/sarif_test.go
@@ -13,37 +13,31 @@ func TestCreateMessageText(t *testing.T) {
 
 	tests := []struct {
 		Name            string
-		RuleName        string
 		FilePath        string
 		ExpectedMessage string
 	}{
 		{
 			Name:            "Filesystem file name",
-			RuleName:        ruleName,
 			FilePath:        "folder/filename.txt",
 			ExpectedMessage: fmt.Sprintf(messagePrefix, "folder/filename.txt"),
 		},
 		{
 			Name:            "Simple git filename",
-			RuleName:        ruleName,
 			FilePath:        "git show 1a9f3c87b4d029f54e8c72d8b11a78f6a3c29d2e:folder/filename.txt",
 			ExpectedMessage: fmt.Sprintf(messagePrefix, "folder/filename.txt"),
 		},
 		{
 			Name:            "Broken git file name with no commit hash",
-			RuleName:        ruleName,
 			FilePath:        "git show folder/filename.txt",
 			ExpectedMessage: fmt.Sprintf(messagePrefix, "git show folder/filename.txt"),
 		},
 		{
 			Name:            "Git file name with one colon character",
-			RuleName:        ruleName,
 			FilePath:        "git show d8e914f06d8d4494bd4f9ab2a2c9c88f78ef25ad:folder/filename:secondpart.txt",
 			ExpectedMessage: fmt.Sprintf(messagePrefix, "folder/filename:secondpart.txt"),
 		},
 		{
 			Name:            "Git file name with multiple colon character",
-			RuleName:        ruleName,
 			FilePath:        "git show a73b5cf94f0b29e1cc6e71a092f6b8ebc1d0e002:folder:secondfolderpart/filename:secondpart.txt",
 			ExpectedMessage: fmt.Sprintf(messagePrefix, "folder:secondfolderpart/filename:secondpart.txt"),
 		},
@@ -51,7 +45,7 @@ func TestCreateMessageText(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
-			message := createMessageText(tt.RuleName, tt.FilePath)
+			message := createMessageText(ruleName, tt.FilePath)
 			fmt.Printf("%v", message)
 			assert.Equal(t, tt.ExpectedMessage, message)
 		})
