b21ebf372e3c64703ec665411795e52528dba943
Júlio Silva <158049359+cx-julio-silva@users.noreply.github.com>
2025-05-09 16:11:26 +0100

    fixed test

--- lib/reporting/sarif_test.go
+++ lib/reporting/sarif_test.go
@@ -8,8 +8,9 @@ import (
 )
 
 func TestCreateMessageText(t *testing.T) {
-	messagePrefix := "Test Rule has detected secret for file %s."
-	
+	ruleName := "Test Rule"
+	messagePrefix := ruleName + " has detected secret for file %s."
+
 	tests := []struct {
 		Name            string
 		RuleName        string
@@ -20,31 +21,31 @@ func TestCreateMessageText(t *testing.T) {
 			Name:            "Filesystem file name",
 			RuleName:        ruleName,
 			FilePath:        "folder/filename.txt",
-			ExpectedMessage: sprintf(messagePrefix, "folder/filename.txt"),
+			ExpectedMessage: fmt.Sprintf(messagePrefix, "folder/filename.txt"),
 		},
 		{
 			Name:            "Simple git filename",
 			RuleName:        ruleName,
 			FilePath:        "git show 1a9f3c87b4d029f54e8c72d8b11a78f6a3c29d2e:folder/filename.txt",
-			ExpectedMessage: sprintf(messagePrefix, "folder/filename.txt"),
+			ExpectedMessage: fmt.Sprintf(messagePrefix, "folder/filename.txt"),
 		},
 		{
 			Name:            "Broken git file name with no commit hash",
 			RuleName:        ruleName,
 			FilePath:        "git show folder/filename.txt",
-			ExpectedMessage: sprintf(messagePrefix, "folder/filename.txt"),
+			ExpectedMessage: fmt.Sprintf(messagePrefix, "git show folder/filename.txt"),
 		},
 		{
 			Name:            "Git file name with one colon character",
 			RuleName:        ruleName,
 			FilePath:        "git show d8e914f06d8d4494bd4f9ab2a2c9c88f78ef25ad:folder/filename:secondpart.txt",
-			ExpectedMessage: sprintf(messagePrefix, "folder/filename:secondpart.txt"),
+			ExpectedMessage: fmt.Sprintf(messagePrefix, "folder/filename:secondpart.txt"),
 		},
 		{
 			Name:            "Git file name with multiple colon character",
 			RuleName:        ruleName,
 			FilePath:        "git show a73b5cf94f0b29e1cc6e71a092f6b8ebc1d0e002:folder:secondfolderpart/filename:secondpart.txt",
-			ExpectedMessage: sprintf(messagePrefix, "folder:secondfolderpart/filename:secondpart.txt"),
+			ExpectedMessage: fmt.Sprintf(messagePrefix, "folder:secondfolderpart/filename:secondpart.txt"),
 		},
 	}
 
