1d713850c49ef26a0763523e41d88a4f449fc1c1
cx-ben-alvo <144705560+cx-ben-alvo@users.noreply.github.com>
2025-06-08 10:30:47 +0300

    fix: update import path to v3 for consistency in createCLI function

--- tests/e2e.go
+++ tests/e2e.go
@@ -21,7 +21,7 @@ type cli struct {
 
 func createCLI(outputDir string) (cli, error) {
 	executable := path.Join(outputDir, "2ms")
-	lib, err := build.Import("github.com/checkmarx/2ms", "", build.FindOnly)
+	lib, err := build.Import("github.com/checkmarx/2ms/v3", "", build.FindOnly)
 	if err != nil {
 		return cli{}, fmt.Errorf("failed to import 2ms: %s", err)
 	}
