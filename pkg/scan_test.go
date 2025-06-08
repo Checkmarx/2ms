be21766a5d67520746f0c716500901dbf4902b93
cx-ben-alvo <144705560+cx-ben-alvo@users.noreply.github.com>
2025-06-08 11:04:14 +0300

    feat: add expected report with validation for scanning results

--- pkg/scan_test.go
+++ pkg/scan_test.go
@@ -19,6 +19,7 @@ const (
 	githubPatPath                           = "testData/secrets/github-pat.txt"
 	jwtPath                                 = "testData/secrets/jwt.txt"
 	expectedReportPath                      = "testData/expectedReport.json"
+	expectedReportWithValidationPath        = "testData/expectedReportWithValidation.json"
 	expectedReportResultsIgnoredResultsPath = "testData/expectedReportWithIgnoredResults.json"
 	expectedReportResultsIgnoredRulePath    = "testData/expectedReportWithIgnoredRule.json"
 )
@@ -561,5 +562,73 @@ func TestScanDynamic(t *testing.T) {
 		actualReport, err := testScanner.ScanDynamic(itemsIn, ScanConfig{})
 		assert.NoError(t, err, "scanner encountered an error")
 		assert.Equal(t, &reporting.Report{Results: map[string][]*secrets.Secret{}}, actualReport)
+	})
+}
+
+func TestScanWithValidation(t *testing.T) {
+	t.Run("Successful Scan with Multiple Items", func(t *testing.T) {
+		cmd.Report = reporting.Init()
+		cmd.SecretsChan = make(chan *secrets.Secret)
+		cmd.SecretsExtrasChan = make(chan *secrets.Secret)
+		cmd.ValidationChan = make(chan *secrets.Secret)
+		cmd.CvssScoreWithoutValidationChan = make(chan *secrets.Secret)
+		cmd.Channels.Items = make(chan plugins.ISourceItem)
+		cmd.Channels.Errors = make(chan error)
+
+		githubPatBytes, err := os.ReadFile(githubPatPath)
+		assert.NoError(t, err, "failed to read github-pat file")
+		githubPatContent := string(githubPatBytes)
+
+		jwtBytes, err := os.ReadFile(jwtPath)
+		assert.NoError(t, err, "failed to read jwt file")
+		jwtContent := string(jwtBytes)
+
+		emptyContent := ""
+		emptyMockPath := "mockPath"
+
+		scanItems := []ScanItem{
+			{
+				Content: &githubPatContent,
+				ID:      fmt.Sprintf("mock-%s", githubPatPath),
+				Source:  githubPatPath,
+			},
+			{
+				Content: &emptyContent,
+				ID:      fmt.Sprintf("mock-%s", emptyMockPath),
+				Source:  emptyMockPath,
+			},
+			{
+				Content: &jwtContent,
+				ID:      fmt.Sprintf("mock-%s", jwtPath),
+				Source:  jwtPath,
+			},
+		}
+
+		testScanner := NewScanner()
+		actualReport, err := testScanner.ScanWithValidation(scanItems, ScanConfig{})
+		assert.NoError(t, err, "scanner encountered an error")
+
+		expectedReportBytes, err := os.ReadFile(expectedReportWithValidationPath)
+		assert.NoError(t, err, "failed to read expected report file")
+
+		var expectedReport, actualReportMap map[string]interface{}
+
+		err = json.Unmarshal(expectedReportBytes, &expectedReport)
+		assert.NoError(t, err, "failed to unmarshal expected report JSON")
+
+		// Marshal actual report and unmarshal back into a map.
+		actualReportBytes, err := json.Marshal(actualReport)
+		assert.NoError(t, err, "failed to marshal actual report to JSON")
+		err = json.Unmarshal(actualReportBytes, &actualReportMap)
+		assert.NoError(t, err, "failed to unmarshal actual report JSON")
+
+		// Normalize both expected and actual maps.
+		normalizedExpectedReport, err := utils.NormalizeReportData(expectedReport)
+		assert.NoError(t, err, "Failed to normalize actual report")
+
+		normalizedActualReport, err := utils.NormalizeReportData(actualReportMap)
+		assert.NoError(t, err, "Failed to normalize actual report")
+
+		assert.EqualValues(t, normalizedExpectedReport, normalizedActualReport)
 	})
 }
