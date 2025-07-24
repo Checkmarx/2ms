0bfa37b0aed44e3c2e301c5a47db44200f5458e5
Rogério Dalot <206845486+cx-rogerio-dalot-x@users.noreply.github.com>
2025-07-24 10:50:15 +0100

    Fixed racing conditions

--- cmd/config_test.go
+++ cmd/config_test.go
@@ -1,12 +1,12 @@
 package cmd
 
 import (
-	"fmt"
+	"testing"
+
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestValidateFormat(t *testing.T) {
@@ -38,20 +38,20 @@ func TestValidateFormat(t *testing.T) {
 			name:            "invalid output format",
 			stdoutFormatVar: "invalid",
 			reportPath:      []string{"report.json"},
-			expectedErr:     fmt.Errorf("invalid output format: invalid, available formats are: json, yaml and sarif"),
+			expectedErr:     errInvalidOutputFormat,
 		},
 		{
 			name:            "invalid report extension",
 			stdoutFormatVar: "json",
 			reportPath:      []string{"report.invalid"},
-			expectedErr:     fmt.Errorf("invalid report extension: invalid, available extensions are: json, yaml and sarif"),
+			expectedErr:     errInvalidReportExtension,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := validateFormat(tt.stdoutFormatVar, tt.reportPath)
-			assert.Equal(t, tt.expectedErr, err)
+			assert.ErrorIs(t, err, tt.expectedErr)
 		})
 	}
 }
