50b0ff984363738888e108f23842d9775043e080
Diogo-fj-rocha <104084969+cx-diogo-rocha@users.noreply.github.com>
2025-11-03 19:12:42 +0000

    Removing unused method in tests

--- engine/rules/rules_test.go
+++ engine/rules/rules_test.go
@@ -1,16 +1,12 @@
 package rules
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/checkmarx/2ms/v4/engine/rules/ruledefine"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/zricethezav/gitleaks/v8/config"
-	"golang.org/x/text/cases"
-	"golang.org/x/text/language"
 )
 
 func TestLoadAllRules(t *testing.T) {
@@ -331,17 +327,5 @@ func TestIgnoreRules(t *testing.T) {
 				}
 			}
 		})
-	}
-}
-
-func TestConvertRuleNames(t *testing.T) {
-	defaultRules := GetDefaultRules(false)
-	for _, rule := range defaultRules {
-		ruleName := rule.RuleName
-		caser := cases.Title(language.English)
-		if strings.Contains(ruleName, "-") {
-			ruleName = caser.String(ruleName)
-		}
-		fmt.Println(ruleName)
 	}
 }
