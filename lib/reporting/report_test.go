af2cdb98c26643eda4197f8253eff3c71ddbbcdb
Diogo-fj-rocha <104084969+cx-diogo-rocha@users.noreply.github.com>
2026-01-19 11:24:08 +0000

    Updated test to use "Jwt" instead of "jwt"

--- lib/reporting/report_test.go
+++ lib/reporting/report_test.go
@@ -516,7 +516,7 @@ func TestGetOutputYAML(t *testing.T) {
 						{
 							ID:               "12fd8706491196cbfbdddd2fdcd650ed842dd963",
 							Source:           "..\\2ms\\pkg\\testData\\secrets\\jwt.txt",
-							RuleName:         "jwt",
+							RuleName:         "Jwt",
 							RuleID:           "0fc98133-a57b-4e08-9990-60952d4a82df",
 							RuleCategory:     "General",
 							StartLine:        1,
@@ -539,7 +539,7 @@ func TestGetOutputYAML(t *testing.T) {
 						{
 							ID:               "12fd8706491196cbfbdddd2fdcd650ed842dd963",
 							Source:           "..\\2ms\\pkg\\testData\\secrets\\jwt.txt",
-							RuleName:         "jwt",
+							RuleName:         "Jwt",
 							RuleID:           "0fc98133-a57b-4e08-9990-60952d4a82df",
 							RuleCategory:     "General",
 							StartLine:        2,
